0b59d732bf607d4c76155ad413e683b288c297c9
Nara Davaasuren <naraamtm@gmail.com>
2022-04-09 15:52:01 -0600

    Changed homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,67 +4,69 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all searches and JOIN with user data
-    const portfolioData = await Portfolio.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const portfolios = portfolioData.map((portfolio) => search.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      portfolios, 
-      logged_in: req.session.logged_in 
-    });
+    if (req.session.logged_in) {
+      res.render('dashboard');
+      return;
+    }
+    else {
+      res.render('homepage');
+    }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/dashboard', async (req, res) => {
+// router.get('/dashboard', async (req, res) => {
+//   try {
+//     const portfolioData = await Portfolio.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     const portfolios = portfolioData.get({ plain: true });
+
+//     res.render('dashboard', {
+//       portfolios,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
-    const portfolioData = await Portfolio.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    const portfolioData = await Portfolio.findAll(
+      { where: { user_id: req.session.user_id } },
+      { include: [{ model: User }] });
 
     const portfolios = portfolioData.get({ plain: true });
 
     res.render('dashboard', {
       portfolios,
-      logged_in: req.session.logged_in
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/ratio', async (req, res) => {
+router.get('/ratio', withAuth, async (req, res) => {
   try {
-    const portfolioData = await Portfolio.findByPk(req.params.id, {
-      include: [
-        {
-          model: Portfolio,
-          attributes: ['name'],
-        },
-      ],
+    const portfolioData = await Portfolio.findAll(
+      { where: { user_id: req.session.user_id } },
+      { include: [{ model: User }] 
     });
 
     const portfolios = portfolioData.get({ plain: true });
 
     res.render('ratio', {
       portfolios,
-      logged_in: req.session.logged_in
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -74,7 +76,7 @@ router.get('/ratio', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/ratio');
     return;
   }
 
