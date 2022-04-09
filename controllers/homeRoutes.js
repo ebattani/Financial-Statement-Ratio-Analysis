5ac846eb6bba07d823437464c4212dbe53a206d1
Ethan Battani <battaniethan@gmail.com>
2022-04-09 13:37:21 -0600

    updated homeRoute to redirect to ratio.hbrs after login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,10 +18,14 @@ router.get('/', async (req, res) => {
     const portfolios = portfolioData.map((portfolio) => search.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      portfolios, 
-      logged_in: req.session.logged_in 
-    });
+
+    if (req.session.logged_in) {
+      res.render('ratio');
+      return;
+    }
+    else{
+    res.render('homepage');
+  }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -64,7 +68,7 @@ router.get('/ratio', async (req, res) => {
 
     res.render('ratio', {
       portfolios,
-      logged_in: req.session.logged_in
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
